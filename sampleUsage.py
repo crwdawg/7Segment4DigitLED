65b0027d139ad38dce042ecd1dfd29feb3b27907
crwdawg <jameswcurtis@yahoo.com>
2021-01-01 11:51:21 -0500

    File to test the class

---
+++ sampleUsage.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from sevenSegmentLED import fourDigitLED
+
+LED = fourDigitLED()
+again = ''
+number = ''
+duration = ''
+try:
+    while again != 'n':
+        number = input("What number would you like to display?  ")
+        duration = input("How long would you like to display the number?  ")
+        LED.display(number,duration)
+        again = input("Do you want to display another number?  (n to quit)")
+        
+
+except KeyboardInterrupt:
+    print("Ctrl-c pressed")
